396af414ebbd0dd94ada06d810369f547f1163d0
omerkonca <omerkonca01@gmail.com>
2026-06-16 17:44:30 +0300

    fix: admin.html syntax error and added auth debug logs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,6 +2,7 @@ const config = require('../config');
 
 function requireAdminToken(req, res, next) {
   const token = req.headers['x-admin-token'];
+  console.log('[auth-debug] Received token:', JSON.stringify(token), 'Expected token:', JSON.stringify(config.ADMIN_TOKEN));
   if (!token || token !== config.ADMIN_TOKEN) {
     return res.status(401).json({
       ok: false,
